5df1db26043d133a7816cd35b492c10c27e2e8e9
olki123 <142659418+olki123@users.noreply.github.com>
2024-09-26 09:56:43 +0100

    Add files via upload

---
+++ starter/script.js
@@ -0,0 +1,46 @@
+// Remember, we're gonna use strict mode in all scripts now!
+/*
+'use strict';
+
+const x = '23';
+
+const calcAge = birthyear => 2037 - birthyear;
+console.log(calcAge(1934));
+
+
+const hasdriverslicense = false; //A
+const hasgoodvision = true; //B
+
+console.log(!hasdriverslicense && hasgoodvision);
+
+const calcAverage = [1 + 2 + 3 + 4 + 5 / 5];
+
+const calcMean = [1, 2, 3, 4, 5];
+
+console.log(calcAverage);
+
+console.log(calcMean);
+
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    value: Number(prompt('Degree celcius:')),
+  };
+  console.log(measurement);
+  console.table(measurement);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+console.log(measureKelvin());
+*/
+
+const data1 = [1, 2, 3, 4];
+const data2 = [6, -9, -5, 3];
+
+console.log(`...${data1[0]}...${data1[1]}...${data1[2]}...`);
+
+console.log(`...${data2[3]}...${data2[2]}..`);
